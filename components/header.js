ca84dcd5ce13146952c43f65417524bae7f44204
Jonah Wasserman <jonahw@optonline.net>
2017-08-31 15:25:50 -0400

    Change attribute casing to follow HTML5 convention

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default () => (
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
+      <meta charset="utf-8" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous" />
       <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
     </Head>
